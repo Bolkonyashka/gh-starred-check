393d3e45596c7d194244496de3eaa78ff6243c3c
Bolkonyashka <romabess55555@yandex.ru>
2018-05-14 19:59:51 +0500

    v1.0

--- githubcheck.py
+++ githubcheck.py
@@ -1,25 +1,32 @@
-import argparse 
+import argparse
 import requests
 import sys
 
-parser = argparse.ArgumentParser(description="Displays all repositories to which the user has set the rating")
-parser.add_argument('username', metavar='username', nargs=1,
-                    help='Username on github.com')
 
-args = parser.parse_args()
-username = args.username[0]
+def get_starred_info():
+    descr = "Displays all repositories to which the user has set the star"
+    parser = argparse.ArgumentParser(description=descr)
+    parser.add_argument('username', type=str, metavar='username',
+                        help='Username on github.com')
 
-response = requests.get('https://api.github.com/users/' + username + '/starred')
-try:
-    response.raise_for_status()
-except requests.exceptions.HTTPError as e:
-    print('Error: ', e)
-    sys.exit()
+    args = parser.parse_args()
+    username = args.username
 
-json_resp = response.json()
-if len(json_resp) == 0:
-    print('User has not starred any repository')
-else:
-    print('The user has starred the following repositories:\r\n')
-    for repo in json_resp:
-        print('%s - %d' % (repo['name'], repo['stargazers_count']))
+    response = requests.get('https://api.github.com/users/' +
+                            username + '/starred')
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print('Error: ', e)
+        sys.exit()
+
+    json_resp = response.json()
+    if len(json_resp) == 0:
+        print('User has not starred any repository')
+    else:
+        print('The user has starred the following repositories:\r\n')
+        for repo in json_resp:
+            print('%s - %d' % (repo['name'], repo['stargazers_count']))
+
+if __name__ == "__main__":
+    get_starred_info()
